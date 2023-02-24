9b76c1f4ce17dc22ae2c808942a63d819505690a
ricardopasqualino <ricardo.pasqualino@gmail.com>
2023-02-24 10:19:14 -0300

    ajuste no settings

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app','teta']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
